26e5721e8779e67cb71d3c433776f8a326c706f5
Fabian Ulbricht <fabianulbricht@web.de>
2023-10-18 19:28:59 +0200

    preparation for general pilot pattern with silence

--- custom_pilots.py
+++ custom_pilots.py
@@ -4,7 +4,44 @@ import numpy as np
 import sionna
 from sionna.ofdm import PilotPattern, KroneckerPilotPattern
 
-# TODO write classe WithSilencePilotPattern which takes other PilotPattern as input and adds silence at specified positions. Might have silence_ind property. Use tf.concat
+# TODO write class PilotPatternWithSilence which takes other PilotPattern as input and adds silence at specified positions. Might have silence_ind property. Use tf.concat
+
+class PilotPatternWithSilence(PilotPattern):
+    """Takes a PilotPattern and adds silence at specified OFDM symbol positions.
+    Can only silence all streams and subcarriers at once.
+    The passed pilot pattern may not have a pilot at any of the silence positions.
+    Does not support training at the moment (silent symbols must be frozen for this).
+
+    Parameters
+    ----------
+    pilot_pattern : PilotPattern
+        Pilot pattern to be used as basis.
+        
+    silent_ofdm_symbol_indices : list of indices (int)
+    """
+    def __init(self, pilot_pattern, silent_ofdm_symbol_indices):
+        self._internal_pilot_pattern = pilot_pattern
+        self._silent_ofdm_symbol_indices = silent_ofdm_symbol_indices
+        mask = np.cast(pilot_pattern.mask.numpy(), dtype=bool)
+        pilots = pilot_pattern.pilots.numpy()
+        
+        # TODO check if there are no pilots at the silent positions
+        
+        # mask ([num_tx, num_streams_per_tx, num_ofdm_symbols, num_effective_subcarriers], bool)
+        mask[:, :, silent_ofdm_symbol_indices, :] = True
+        # pilots ([num_tx, num_streams_per_tx, num_pilots], tf.complex)
+        for silent_index in silent_ofdm_symbol_indices:
+            print("TODO")
+
+        super.__init__(mask, pilots, trainable=False, normalize=False,
+                         dtype=pilot_pattern._dtype)
+    
+    @property
+    def silent_ofdm_symbol_indices(self):
+        return self._silent_ofdm_symbol_indices
+    
+    
+    
 class OneHotWithSilencePilotPattern(PilotPattern):
     """Creates one-hot pilot pattern. Per stream one OFDM symbol.
     After the pilots, silence is transmitted.
