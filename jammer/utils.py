covariance estimation corrected: broadcast over all odfm symbols

jammer/utils.py
@@ -31,13 +31,17 @@ def _constellation_to_sampler(constellation, dtype):
     return sampler
 
 
-def covariance_estimation_from_signals(y):
+def covariance_estimation_from_signals(y, num_odfm_symbols):
     """Estimate the covariance matrix of a signal y.
     y: [batch_size, num_rx, num_rx_ant, num_jammer_symbols, fft_size], tf.complex
-    output: [batch_size, num_rx, num_jammer_symbols, fft_size, num_rx_ant, num_rx_ant]
-    where num_jammer_symbols is the number of symbols where only the jammer is sending
+        where num_jammer_symbols is the number of symbols where only the jammer is sending
+    num_ofdm_symbols: int
+    output: [batch_size, num_rx, num_ofdm_symbols, fft_size, num_rx_ant, num_rx_ant]
     """
     # we calculate the covariance matrix, broadcast it over all ofdm symbols
     # TODO broadcast over all ofdm symbols!!!!!!
     y = tf.transpose(y, perm=[0, 1, 4, 2, 3]) # [batch_size, num_rx, fft_size, num_rx_ant, num_jammer_symbols]
-    return tf.matmul(y, y, adjoint_b=True)/tf.cast(tf.shape(y)[-1], y.dtype)
+    # 1/N * y*y^H
+    cov = tf.matmul(y, y, adjoint_b=True)/tf.cast(tf.shape(y)[-1], y.dtype)
+    # add num_ofdm_symbols dimension by repeating
+    return tf.tile(cov[:, :, tf.newaxis, :, :, :], [1, 1, num_odfm_symbols, 1, 1, 1])

jammer_simulation.py
@@ -12,7 +12,7 @@ if gpus:
     except RuntimeError as e:
         print(e)
 tf.get_logger().setLevel('ERROR')
-tf.config.run_functions_eagerly(True)
+# tf.config.run_functions_eagerly(True)
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -231,7 +231,7 @@ class Model(tf.keras.Model):
                 # TODO: one of the next 2 lines is slow. Benchmark and optimize. Might be tf.gather. Should we only allow connected slices?
                 # TODO not working. Maybe use sampler always giving 1, estimated covariance should then be very close to real covariance
                 jammer_signals = tf.gather(y, self._silent_pilot_symbol_indices, axis=3)
-                jammer_covariance = covariance_estimation_from_signals(jammer_signals)
+                jammer_covariance = covariance_estimation_from_signals(jammer_signals, self._num_ofdm_symbols)
             if self._jammer_mitigation == "pos":
                 if self._return_jammer_csi:
                     self._pos.set_jammer(j)
@@ -300,9 +300,9 @@ model_parameters = {
 # model_parameters["jammer_present"] = True
 # simulate("LMMSE with Jammer")
 
-model_parameters["jammer_present"] = True
-model_parameters["jammer_mitigation"] = "pos"
-simulate("LMMSE with Jammer, POS")
+# model_parameters["jammer_present"] = True
+# model_parameters["jammer_mitigation"] = "pos"
+# simulate("LMMSE with Jammer, POS")
 
 model_parameters["jammer_present"] = True
 model_parameters["jammer_mitigation"] = "ian"