fix non-trainable weight matrix, gradient being stopped at variable

jammer/jammer.py
@@ -51,12 +51,12 @@ class OFDMJammer(tf.keras.layers.Layer):
             # all weights are trainable
             jammer_input_shape = tf.concat([[self._num_tx, self._num_tx_ant], input_shape[0][-2:]], axis=0)
             self._training_weights = tf.Variable(tf.ones(jammer_input_shape), dtype=self._dtype_as_dtype.real_dtype, trainable=self.trainable, constraint=constraint)
-            self._weights = tf.Variable(tf.ones(jammer_input_shape))
+            self._weights = tf.Variable(tf.ones(jammer_input_shape), trainable=False)
         else:
             self._training_indices = tf.where(self._trainable_mask)
             count_trainable = tf.shape(self._training_indices)[0]
             self._training_weights = tf.Variable(tf.ones([count_trainable]), dtype=self._dtype_as_dtype.real_dtype, trainable=self.trainable, constraint=constraint)
-            self._weights = tf.Variable(tf.ones(self._trainable_mask.shape))
+            self._weights = tf.Variable(tf.ones(self._trainable_mask.shape), trainable=False)
         # below: weights only over ofdm_symbols
         # num_ofdm_symbols = input_shape[0][-2]
         # constraint = NonNegMaxMeanSquareNorm(1)
@@ -73,12 +73,13 @@ class OFDMJammer(tf.keras.layers.Layer):
         x_jammer = self.sample(jammer_input_shape)
         # weights have mean(|w|^2) <= 1
         if self._trainable_mask is None:
-            # weights = self._training_weights
-            self._weights.assign(self._training_weights)
+            weights = self._training_weights
+            self._weights.assign(weights)
         else:
-            # weights = tf.tensor_scatter_nd_update(tf.ones(self._trainable_mask.shape), self._training_indices, self._training_weights)
-            self._weights.assign(tf.tensor_scatter_nd_update(tf.ones(self._trainable_mask.shape), self._training_indices, self._training_weights))
-        x_jammer = tf.cast(self._weights, x_jammer.dtype) * x_jammer
+            weights = tf.tensor_scatter_nd_update(tf.ones(self._trainable_mask.shape), self._training_indices, self._training_weights)
+            self._weights.assign(weights)
+        x_jammer = tf.cast(weights, x_jammer.dtype) * x_jammer
+        # x_jammer = tf.cast(weights, x_jammer.dtype) * x_jammer
         rho = self.make_sparse(rho, tf.shape(x_jammer))
 
         x_jammer = tf.sqrt(rho) * x_jammer

jammer_simulation.py
@@ -636,26 +636,32 @@ model_parameters["jammer_present"] = True
 model_parameters["jammer_mitigation"] = "pos"
 model_parameters["jammer_mitigation_dimensionality"] = 1
 jammer_parameters["trainable"] = True
+
 # jammer which sends during jammer-pilots, but is able to learn during rest
-filename = "datalearning_weights.pickle"
-jammer_parameters["trainable_mask"] = tf.concat([tf.zeros([4,128], dtype=bool), tf.ones([10,128], dtype=tf.bool)], axis=0)
-model_train = Model(**model_parameters)
-train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
-# jammer which can choose any rg-element to send on
-filename = "whole_rg_weights.pickle"
-jammer_parameters["trainable_mask"] = tf.ones([14,128], dtype=bool)
-model_train = Model(**model_parameters)
-train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+# filename = "datalearning_weights.pickle"
+# jammer_parameters["trainable_mask"] = tf.concat([tf.zeros([4,128], dtype=bool), tf.ones([10,128], dtype=tf.bool)], axis=0)
+# model_train = Model(**model_parameters)
+# train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+
+# # jammer which can choose any rg-element to send on
+# filename = "whole_rg_weights.pickle"
+# jammer_parameters["trainable_mask"] = tf.ones([14,128], dtype=bool)
+# model_train = Model(**model_parameters)
+# train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+
 # jammer which can only choose symbol times
 filename = "symbol_weights.pickle"
-jammer_parameters["trainable_mask"] = tf.ones([14], dtype=bool)
+jammer_parameters["trainable_mask"] = tf.ones([14, 1], dtype=bool)
 model_train = Model(**model_parameters)
-train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
-# jammer which can choose non-silent symbol times
-filename = "nonsilent_symbol_weights.pickle"
-jammer_parameters["trainable_mask"] = tf.concat([tf.zeros([4], tf.ones([10], dtype=bool), axis=0)], axis=0))
-model_train = Model(**model_parameters)
-train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+# train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+train_model(model_train, 5000, filename, log_tensorboard=True)
+
+# # jammer which can choose non-silent symbol times
+# filename = "nonsilent_symbol_weights.pickle"
+# jammer_parameters["trainable_mask"] = tf.concat([tf.zeros([4, 1], dtype=bool), tf.ones([10, 1], dtype=bool)], axis=0)
+# model_train = Model(**model_parameters)
+# train_model(model_train, 40000, filename, log_tensorboard=True, log_weight_images=True)
+
 # # inference
 # jammer_parameters["trainable"] = False
 # simulate("Untrained Jammer")