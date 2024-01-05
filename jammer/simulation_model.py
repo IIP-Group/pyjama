b3c63421009668003f601e78d3a659275feb351a
Fabian Ulbricht <fabianulbricht@web.de>
2024-01-05 18:37:58 +0100

    prototype of gpu-data-parallel inference (not working yet)

--- jammer/simulation_model.py
+++ jammer/simulation_model.py
@@ -1,19 +1,19 @@
 #%%
 import os
 # import drjit
-gpu_num = 0 # Use "" to use the CPU
-os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_num}"
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# gpu_num = 0 # Use "" to use the CPU
+# os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_num}"
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import sionna
 import tensorflow as tf
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
-tf.get_logger().setLevel('ERROR')
+# gpus = tf.config.list_physical_devices('GPU')
+# if gpus:
+#     try:
+#         for gpu in gpus:
+#             tf.config.experimental.set_memory_growth(gpu, True)
+#     except RuntimeError as e:
+#         print(e)
+# tf.get_logger().setLevel('ERROR')
 # tf.config.run_functions_eagerly(True)
 
 import matplotlib.pyplot as plt
--- simulations/tests.py
+++ simulations/tests.py
@@ -1,7 +1,7 @@
 # %%
 import os
-# import drjit
-gpu_num = 0 # Use "" to use the CPU
+import drjit
+gpu_num = [0, 1, 2, 3] # Use "" to use the CPU
 os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_num}"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import sionna
@@ -17,33 +17,42 @@ tf.get_logger().setLevel('ERROR')
 # tf.config.run_functions_eagerly(True)
 
 from jammer.simulation_model import *
+import jammer.simulation_model as sim
 from tensorflow.python.keras.losses import MeanAbsoluteError, MeanSquaredError, BinaryCrossentropy
 
+strategy = tf.distribute.MirroredStrategy()
 
-# common parameters
-model_parameters["perfect_csi"] = False
-model_parameters["jammer_present"] = True
-model_parameters["jammer_mitigation"] = "pos"
-model_parameters["jammer_mitigation_dimensionality"] = 1
-jammer_parameters["trainable"] = True
-model_parameters["return_symbols"] = True
+def computation():
+    # common parameters
+    model_parameters = {}
+    jammer_parameters = {}
+    model_parameters["perfect_csi"] = False
+    model_parameters["num_ut"] = 1
+    model_parameters["jammer_present"] = True
+    model_parameters["jammer_power"] = 1.0
+    model_parameters["jammer_mitigation"] = "pos"
+    model_parameters["jammer_mitigation_dimensionality"] = 1
+    model_parameters["num_silent_pilot_symbols"] = 4
+    jammer_parameters["trainable"] = True
+    model_parameters["jammer_parameters"] = jammer_parameters
 
-# needed, as keras MSE does not take |.|
-abs_sqrt = lambda y_true, y_pred: tf.reduce_mean(tf.sqrt(tf.abs(y_true - y_pred)))
-abs_log = lambda y_true, y_pred: tf.reduce_mean(tf.math.log(tf.abs(y_true - y_pred) + 1))
-# name, loss_fn, over symbols?, loss_over_logits
-parameters = [
-    ("log over bit estimates", negative_function(abs_log), False, False),
-    ("sqrt over bit estimates", negative_function(abs_sqrt), False, False),
-]
+    ber_plots.reset()
+    sim.MAX_MC_ITER = 100
+    sim.BATCH_SIZE = 16
+    jammer_parameters["trainable"] = False
+    jammer_parameters["trainable_mask"] = tf.ones([14, 128])
+    snrs = np.arange(-5.0, 10.5, 2.5)
+    for snr in snrs:
+        filename = f"weights/{snr}dB_relufix.pickle"
+        model = Model(**model_parameters)
+        load_weights(model, filename)
+        simulate_model(model, f"{snr}dB, abs.")
+    for snr in snrs:
+        filename = f"weights/{snr}dB_quadratic_0.5co.pickle"
+        model = Model(**model_parameters)
+        load_weights(model, filename)
+        simulate_model(model, f"{snr}dB, quadratic cutoff")
+    ber_plots.title = "Absolute vs. Quadratic Cutoff (0.5) Loss"
+    ber_plots()
 
-for name, loss_fn, over_symbols, loss_over_logits in parameters:
-    model_parameters["return_symbols"] = over_symbols
-    model = Model(**model_parameters)
-    train_model(model,
-                loss_fn=loss_fn,
-                loss_over_logits=loss_over_logits,
-                weights_filename=f"weights/{name}.pickle",
-                log_tensorboard=True,
-                log_weight_images=True,
-                show_final_weights=True)
+strategy.run(computation)
