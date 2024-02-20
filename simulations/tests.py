b4aa339a5784cda5396e905234c33485a8d9558b
Fabian Ulbricht <fabianulbricht@web.de>
2024-02-20 23:39:40 +0100

    some new graphs

--- simulations/tests.py
+++ simulations/tests.py
@@ -21,30 +21,31 @@ import jammer.simulation_model as sim
 from tensorflow.python.keras.losses import MeanAbsoluteError, MeanSquaredError, BinaryCrossentropy
 
 # common parameters
-model_parameters = {}
-jammer_parameters = {}
-model_parameters["scenario"] = "umi"
-model_parameters["perfect_csi"] = False
-model_parameters["num_ut"] = 2
-model_parameters["num_ut_ant"] = 2
-model_parameters["jammer_present"] = True
-model_parameters["jammer_power"] = db_to_linear(-3.)
-# model_parameters["jammer_mitigation"] = "pos"
-# model_parameters["jammer_mitigation_dimensionality"] = 1
-model_parameters["num_silent_pilot_symbols"] = 4
-model_parameters["jammer_parameters"] = jammer_parameters
+# model_parameters = {}
+# jammer_parameters = {}
+# model_parameters["scenario"] = "umi"
+# model_parameters["perfect_csi"] = False
+# model_parameters["num_ut"] = 2
+# model_parameters["num_ut_ant"] = 2
+# model_parameters["jammer_present"] = True
+# model_parameters["jammer_power"] = db_to_linear(-3.)
+# # model_parameters["jammer_mitigation"] = "pos"
+# # model_parameters["jammer_mitigation_dimensionality"] = 1
+# model_parameters["num_silent_pilot_symbols"] = 4
+# model_parameters["jammer_parameters"] = jammer_parameters
 
-sim.BATCH_SIZE = 8
-sim.MAX_MC_ITER = 50
+# sim.BATCH_SIZE = 8
+# sim.MAX_MC_ITER = 50
 
-model_parameters["jammer_present"] = True
-model_parameters["mash"] = False
-model = Model(**model_parameters)
-simulate_model(model, "jammer")
+# model_parameters["jammer_present"] = True
+# model_parameters["mash"] = False
+# model = Model(**model_parameters)
+# simulate_model(model, "jammer")
 
-model_parameters["jammer_present"] = True
-model_parameters["mash"] = True
-model = Model(**model_parameters)
-simulate_model(model, "jammer, MASH")
+# model_parameters["jammer_present"] = True
+# model_parameters["mash"] = True
+# model = Model(**model_parameters)
+# simulate_model(model, "jammer, MASH")
 
-ber_plots()
+# ber_plots()
+rg = sionna.ofdm.ResourceGrid(14, 128, 30e3, 2, 2, pilot_pattern='kronecker', pilot_ofdm_symbol_indices=[0, 1])
